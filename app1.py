bb8f572c17edb2315c5985fe8993a2d130848de4
NAMITHA-L <leninnamitha@gmail.com>
2025-11-23 15:07:43 +0530

    Name routing

--- app1.py
+++ app1.py
@@ -1,9 +1,9 @@
 from flask import Flask,render_template
 app1 = Flask(__name__)
 
-@app1.route("/")
-def home():
-    return render_template("home.html")
+@app1.route("/<name>")
+def home(name):
+    return render_template("home.html",user_name = name)
 
 @app1.route("/about")
 def about():
