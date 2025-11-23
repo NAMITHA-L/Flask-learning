d3642f917efb16b7ba479a7b70ba780ef4be758e
NAMITHA-L <leninnamitha@gmail.com>
2025-11-23 20:55:00 +0530

    for loop

--- app1.py
+++ app1.py
@@ -1,9 +1,10 @@
 from flask import Flask,render_template
 app1 = Flask(__name__)
 
-@app1.route("/<name>")
-def home(name):
-    return render_template("home.html",user_name = name)
+@app1.route("/")
+def home():
+    marks = [23,12,34,45,56,67,78,89,90]
+    return render_template("home.html",List = marks)
 
 @app1.route("/about")
 def about():
