490935d16d345554451b8dcafae92107d8a53a8d
NAMITHA-L <leninnamitha@gmail.com>
2025-11-27 20:45:31 +0530

    getting data from a form

--- app1.py
+++ app1.py
@@ -3,17 +3,17 @@ app1 = Flask(__name__)
 
 @app1.route("/",methods = ['GET'])
 def home():
+    success = ""
     try:
-     print(request.args)
-     data = request.args
-     print(data)
-     print(type(data))
-     my_name = request.args.get('name')
-     print(my_name )
-     my_age = request.args.get('age')
-     print(my_age+"hi") 
-     print(my_age+90)
-     return render_template("home.html")
+        if request.args.get('reg') == "Register":
+            name = request.args.get('name')
+            email = request.args.get('email')
+            pwd = request.args.get('pwd')
+            print(name, email,pwd)
+            success = "success"
+            return render_template("form.html", success=success )
+        else:
+            return render_template("form.html",success=success)
     except Exception as e:
        return "Provide valid data"
        #raise
@@ -25,8 +25,6 @@ def about():
 @app1.route("/profile")
 def profile():
     return render_template("profile.html")
-
-
 
 if __name__ == "__main__":
     app1.run(debug = True)
