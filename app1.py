a9c9f6ff66162bd4fecba64904f6dce57cc17d14
NAMITHA-L <leninnamitha@gmail.com>
2025-11-30 22:45:00 +0530

    design dasboard

--- app1.py
+++ app1.py
@@ -14,24 +14,25 @@ cursor = db.cursor()
 
 @app1.route("/",methods = ['GET'])
 def home():
-    success = ""
-    try:
-        if request.args.get('reg') == "Register":
-            name = request.args.get('name')
-            email = request.args.get('email')
-            pwd = request.args.get('pwd')
-            print(name, email,pwd)
-            sql = "insert into users(name,email,pwd) values('{0}','{1}','{2}')".format(name,email,pwd)
-            cursor.execute(sql)
-            db.commit()
-            if cursor.rowcount >= 1:
-                            success = "success"
-            return render_template("form.html", success=success )
-        else:
-            return render_template("form.html",success=success)
-    except Exception as e:
-       return "Provide valid data"
-       #raise
+    return render_template("home.html")
+    #success = ""
+    #try:
+        # if request.args.get('reg') == "Register":
+        #    name = request.args.get('name')
+        #   email = request.args.get('email')
+        #   pwd = request.args.get('pwd')
+        #   print(name, email,pwd)
+        #   sql = "insert into users(name,email,pwd) values('{0}','{1}','{2}')".format(name,email,pwd)
+        #   cursor.execute(sql)
+        #   db.commit()
+        #   if cursor.rowcount >= 1:
+        #                   success = "success"
+        #  return render_template("form.html", success=success )
+        #else:
+        #    return render_template("form.html",success=success)
+    #except Exception as e:
+    #   return "Provide valid data"
+       #raise`
 
 @app1.route("/about")
 def about():
